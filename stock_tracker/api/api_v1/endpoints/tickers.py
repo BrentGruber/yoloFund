017655c9267e380187563e801e6d7d0a781dc0dd
Brent Gruber <brent.gruber77@gmail.com>
2021-02-14 23:53:55 -0500

    parameterizing subreddit name

--- stock_tracker/api/api_v1/endpoints/tickers.py
+++ stock_tracker/api/api_v1/endpoints/tickers.py
@@ -22,13 +22,13 @@ async def read_exchanges(skip: int = 0, limit: int = 100) -> Any:
     return await Ticker_Pydantic.from_queryset(Ticker.all().offset(skip).limit(limit))
 
 @router.get("/ticker-mentions")
-async def check_tickers():
+async def check_tickers(subredddit: str = "RobinhoodPennyStocks"):
     r = await Reddit.create()
 
     tickers = await Ticker_Pydantic.from_queryset(Ticker.all())
     tickers = [t.symbol for t in tickers]
 
-    mentions = await r.get_top_tickers("RobinhoodPennyStocks", tickers)
+    mentions = await r.get_top_tickers(subredddit, tickers)
     mentions = [{"name": k, "count": v.get("count")} for k,v in mentions.items()]
 
     return sorted(mentions, key = lambda i: i["count"], reverse=True)
