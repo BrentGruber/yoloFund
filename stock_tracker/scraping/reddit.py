bfb7204c976ff3b1ba8c2c9cf89e7a3ef2a16868
Brent Gruber <brent.gruber77@gmail.com>
2021-02-15 00:40:32 -0500

    removing old code and fixing bug where mentions was being set to None

--- stock_tracker/scraping/reddit.py
+++ stock_tracker/scraping/reddit.py
@@ -14,9 +14,6 @@ class Reddit():
     async def create(cls):
         """
         Initialize the class with an asyncpraw connection
-
-        Args:
-            settings ([type]): [description]
         """
         self = Reddit()
 
@@ -54,7 +51,7 @@ class Reddit():
         
         return retval
 
-    async def _get_comment_tickers(self, comment: asyncpraw.models.Comment, tickers: List[str]):
+    async def _get_comment_tickers(self, comment: asyncpraw.models.Comment, tickers: List[str]) -> Dict[str, dict]:
         """
         Parses a single comment in a reddit post and counts the number of times a regular expression occurs
 
@@ -64,13 +61,15 @@ class Reddit():
 
         Returns:
             Dict[str, int]: A dictionary which maps the stock ticker to the count of times it occurs
+
+        TODO: incorporate pydantic models for return values
         """
         retval = {}
 
         for phrase in re.findall(self.ticker_re, comment.body):
             if phrase not in self.blacklist and phrase in tickers:
                 if retval.get(phrase):
-                    retval[phrase]["mentions"] = retval.get('mentions',[]).append({"location": "comment", "id": comment.id})
+                    retval[phrase]["mentions"] += [{"location": "comment", "id": comment.id}]
                     retval[phrase]["count"] = retval.get(phrase).get("count", 0) + 1
                 else:
                     retval[phrase] = {
@@ -80,7 +79,7 @@ class Reddit():
         
         return retval
 
-    async def _get_submission_tickers(self, submission: asyncpraw.models.Submission, tickers: List[str]):
+    async def _get_submission_tickers(self, submission: asyncpraw.models.Submission, tickers: List[str]) -> Dict[str, dict]:
         """
         Parses a submission in reddit and counts the number of times a regular expression occurs
 
@@ -90,6 +89,8 @@ class Reddit():
 
         Returns:
             Dict[str, int]: Dictionary with count of occurrences for each ticker
+
+        TODO: incorporate pydantic models for return values
         """
         retval = {}
 
@@ -98,7 +99,7 @@ class Reddit():
         for phrase in re.findall(self.ticker_re, submission.title):
             if phrase not in self.blacklist and phrase in tickers:
                 if retval.get(phrase):
-                    retval[phrase]["mentions"] = retval.get('mentions',[]).append({"location": "submission", "id": submission.id})
+                    retval[phrase]["mentions"] += [{"location": "submission", "id": submission.id}]
                     retval[phrase]["count"] = retval.get(phrase).get("count", 0) + 1
                 else:
                     retval[phrase] = {
@@ -138,7 +139,7 @@ class Reddit():
                 else:
                     #symbol has not already been created, so create it
                     retval[symbol] = {
-                        "mentions": details.get("mentions", []),
+                        "mentions": details.get("mentions", []) or [],
                         "count": details.get("count")
                     }
 
@@ -194,49 +195,3 @@ class Reddit():
         """
         sub = await self.reddit.subreddit(subreddit)
         return await self._get_subreddit_tickers(sub, tickers)
-
-
-# async def get_ticker_mentions(subreddit: str, tickers: List[str]):
-#     """""
-#     Searches a subreddit to retrieve the top tickers mentioned
-
-#     Args:
-#         subreddit (str): subreddit to scrape
-#         tickers (List[str]): list of tickers to search for
-
-#     Returns:
-#         [type]: [description]
-#     """
-#     ssl_ctx = ssl._create_unverified_context()
-
-#     conn = aiohttp.TCPConnector(ssl_context=ssl_ctx)
-#     session = aiohttp.ClientSession(connector=conn)
-#     reddit = asyncpraw.Reddit(
-#         client_id=settings.REDDIT_CLIENT_ID,
-#         client_secret=settings.REDDIT_CLIENT_SECRET,
-#         user_agent="YoloFund",
-#         requestor_kwargs={"session": session},
-#     )
-#     dailyTickers = {}
-
-#     regexPattern = r'\b([A-Z]+)\b'
-#     blacklist = 
-#     sub = await reddit.subreddit(subreddit)
-
-#     async for submission in sub.top("day"):
-#         print(submission)
-#         strings = [submission.title]
-#         comments = await submission.comments()
-#         await comments.replace_more(limit=0)
-#         async for comment in comments:
-#             strings.append(comment.body)
-#         for s in strings:
-#             for phrase in re.findall(regexPattern, s):
-#                 if phrase not in blacklist:
-#                     if phrase in tickers:
-#                         dailyTickers[phrase] = dailyTickers.get(phrase, 0) + 1
-
-#     return dailyTickers
-
-
-#     return True
